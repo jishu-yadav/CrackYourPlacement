589574b5c8fcc9de8b55662c3f36cd04e57681b6
jishu-yadav <54631311+jishu-yadav@users.noreply.github.com>
2022-05-05 00:14:09 +0530

    Create q4.cpp

---
+++ Day1/q4.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        if(prices.size()<2)
+            return 0;
+        int profit=0,buy=prices[0];
+        for(int i=1;i<prices.size();i++){
+            buy=min(buy,prices[i]);
+            profit=max(profit,prices[i]-buy);
+        }
+        return profit;
+        
+    }
+};
