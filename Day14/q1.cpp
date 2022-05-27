9451b5529a714cb0066335a2392736b0865a422a
jishu-yadav <54631311+jishu-yadav@users.noreply.github.com>
2022-05-28 01:37:37 +0530

    Create q1.cpp

---
+++ Day14/q1.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int arr[9000];
+    int n,i,val,diff;
+ 
+    while(1){
+        int max_load=0,load=0;
+        cin>>n;
+        if(n==-1)
+        break;
+        for(int i=0;i<n;i++){
+            cin>>arr[i];
+            load+=arr[i];
+        }
+        if(load%n!=0){
+            cout<<-1<<endl;
+            continue;
+        }
+        load = load/n;
+        diff=0;
+    //greedy step
+        for(int i=0;i<n;i++){
+            diff += (arr[i]-load);
+            int ans = max(diff,-diff);
+            max_load = max(max_load, ans);
+        }
+        cout<<max_load<<endl;
+        n=-1;//as i gave only one case
+    }
+    return 0;
+}
+//load balancer greedy
