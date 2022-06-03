31c00f83255ab824c117cacbadbde3c49fa2bcd7
Jishu Yadav <54631311+jishu-yadav@users.noreply.github.com>
2022-06-03 19:06:49 +0530

    Time: 110 ms (91.73%), Space: 53.4 MB (73.14%) - LeetHub

---
+++ 131-palindrome-partitioning/131-palindrome-partitioning.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    bool isPallin(string s,int i,int j){
+        while(i<j){
+            if(s[i++]!=s[j--]){
+                return false;
+            }
+        }
+        return true;
+    }
+    void dfs(int start,string &s,vector<vector<string>>&ans,vector<string>&p){
+        if(start>=s.size()) ans.push_back(p);
+        for(int end = start;end<s.size();end++){
+           // string checkPallin = s.substr(start,end);
+            if(isPallin(s,start,end)){
+                p.push_back(s.substr(start,end-start+1));
+                dfs(end+1,s,ans,p);
+                p.pop_back();
+            }
+        }
+        
+    }
+    vector<vector<string>> partition(string s) {
+        vector<vector<string>>ans;
+        vector<string>p;
+        dfs(0,s,ans,p);
+        return ans;
+        
+    }
+};
