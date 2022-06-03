61d2b0a4bbbc3a986ca7538bd0a9d31ac0db9db9
Jishu Yadav <54631311+jishu-yadav@users.noreply.github.com>
2022-06-03 22:03:56 +0530

    Time: 3 ms (58.86%), Space: 6 MB (48.83%) - LeetHub

--- 60-permutation-sequence/60-permutation-sequence.cpp
+++ 60-permutation-sequence/60-permutation-sequence.cpp
@@ -2,22 +2,21 @@ class Solution {
 public:
     string getPermutation(int n, int k) {
         int fact=1;
-        string ans="";
+        string ans = "";
         vector<int>numbers;
         for(int i=1;i<n;i++){
             fact*=i;
             numbers.push_back(i);
         }
         numbers.push_back(n);
-        k = k-1;
+        k=k-1;
         while(true){
-            ans+=to_string(numbers[k/fact]);
+            ans += to_string(numbers[k/fact]);
             numbers.erase(numbers.begin()+k/fact);
-            if(numbers.size()==0){
+            if(numbers.size()==0)
                 break;
-            }
             k=k%fact;
-            fact = fact/numbers.size();
+            fact=fact/numbers.size();
         }
         return ans;
     }
