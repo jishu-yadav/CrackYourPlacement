8a1ad563ca0348d5823dd04f63689223260ec6c4
Jishu Yadav <54631311+jishu-yadav@users.noreply.github.com>
2022-06-08 00:03:16 +0530

    Time: 14 ms (89.32%), Space: 13.8 MB (42.83%) - LeetHub

---
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool static compare(pair<int,int>&p1,pair<int,int>&p2) {
+        return p1.second > p2.second;
+    }
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+       unordered_map<int, int> hash;
+        vector<pair<int, int>> v;
+        vector<int> ans;
+        //Setup the hashtable/hashmap to freq of occurrence of values from nums
+        for(auto n:nums) {
+            hash[n]++;
+        }
+      
+        // Make pair of Value and its frequency
+        for(auto i:hash) {
+            v.push_back({i.first, i.second});
+        }
+        // Sort in desc order for frequencies
+        sort(v.begin(), v.end(), compare);
+        // Store top k repeating nums in ans
+        for(int p=0; p<k; p++) {
+            ans.push_back(v[p].first);
+        }
+        return ans; 
+    }
+};
