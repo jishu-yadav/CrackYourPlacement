b504122ce2b82ec2e40fff721264963a83a37bc9
Jishu Yadav <54631311+jishu-yadav@users.noreply.github.com>
2022-06-07 19:31:27 +0530

    Time: 3 ms (72.68%), Space: 8.9 MB (99.28%) - LeetHub

--- 88-merge-sorted-array/88-merge-sorted-array.cpp
+++ 88-merge-sorted-array/88-merge-sorted-array.cpp
@@ -1,10 +1,39 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        int x=0;
-        for(int i=m;i<m+n;i++){
-            nums1[i] = nums2[x++];
+        int x=m+n-1;
+        // for(int i=m;i<m+n;i++){
+        //     nums1[i] = nums2[x++];
+        // }
+        // sort(nums1.begin(),nums1.end());   
+        int idx1=m-1;
+        int idx2=n-1;
+        while(idx1>=0 && idx2>=0){
+            if(nums1[idx1]<nums2[idx2]){
+                nums1[x--] = nums2[idx2--];
+            }
+            else{
+                nums1[x--] = nums1[idx1--];
+            }
         }
-        sort(nums1.begin(),nums1.end());   
+//         while(idx1>=0){
+//             if(nums1[idx1]<nums2[idx2]){
+//                 nums1[x--] = nums2[idx2--];
+//             }
+//             else{
+//                 nums1[x--] = nums1[idx1--];
+//             }
+//         }
+        
+        while(idx2>=0){
+            nums1[x--]=nums2[idx2--];
+            // if(nums1[idx1]<nums2[idx2]){
+            //     nums1[x--] = nums2[idx2--];
+            // }
+            // else{
+            //     nums1[x--] = nums1[idx1--];
+            // }
+        }
+        
     }
 };
