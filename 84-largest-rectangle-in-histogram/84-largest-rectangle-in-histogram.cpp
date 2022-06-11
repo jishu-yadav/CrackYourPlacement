4d4940b587622fd50860b4a82b9a394f28e3490e
Jishu Yadav <54631311+jishu-yadav@users.noreply.github.com>
2022-06-11 17:47:22 +0530

    Time: 210 ms (72.81%), Space: 75.3 MB (90.99%) - LeetHub

--- 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.cpp
+++ 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.cpp
@@ -1,41 +1,56 @@
 class Solution {
 public:
-    int largestRectangleArea(vector<int>& heights) {
-        int n = heights.size();
-        int leftSmall[n],rightSmall[n];
+    int largestRectangleArea(vector<int>& histo) {
+//         int n = heights.size();
+//         int leftSmall[n],rightSmall[n];
+//         stack<int>st;
+        
+//         //search for left small
+//         for(int i=0;i<n;i++){
+//             while(!st.empty() && heights[i]<=heights[st.top()] ){
+//                 st.pop();
+//             }
+//             if(st.empty())
+//                 leftSmall[i] = 0;
+//             else
+//                 leftSmall[i] = st.top()+1; 
+//             st.push(i);
+//         }
+//         //making the stack empty to use it again
+//         while(!st.empty()){
+//             st.pop();
+//         }
+        
+//          for(int i=n-1;i>=0;i--){
+//             while(!st.empty() && heights[i]<=heights[st.top()] ){
+//                 st.pop();
+//             }
+//             if(st.empty())
+//                 rightSmall[i] = n-1;
+//             else
+//                 rightSmall[i] = st.top()-1; 
+//             st.push(i);
+//         }
+//         int largestArea=0;
+//         for(int i=0;i<n;i++){
+//             largestArea = max(largestArea, (rightSmall[i] - leftSmall[i] + 1)*heights[i]);
+//         }
+//         return largestArea;
+        // 2nd approach
         stack<int>st;
-        
-        //search for left small
-        for(int i=0;i<n;i++){
-            while(!st.empty() && heights[i]<=heights[st.top()] ){
+        int maxA=0;
+        int n = histo.size();
+        for(int i=0;i<=n;i++){
+            while(!st.empty() && (i==n || histo[st.top()]>=histo[i])){
+                int height = histo[st.top()];
                 st.pop();
+                int width;
+                if(st.empty()) width = i;
+                else width = i- st.top() - 1;
+                maxA = max(maxA, width*height);
             }
-            if(st.empty())
-                leftSmall[i] = 0;
-            else
-                leftSmall[i] = st.top()+1; 
             st.push(i);
         }
-        //making the stack empty to use it again
-        while(!st.empty()){
-            st.pop();
-        }
-        
-         for(int i=n-1;i>=0;i--){
-            while(!st.empty() && heights[i]<=heights[st.top()] ){
-                st.pop();
-            }
-            if(st.empty())
-                rightSmall[i] = n-1;
-            else
-                rightSmall[i] = st.top()-1; 
-            st.push(i);
-        }
-        int largestArea=0;
-        for(int i=0;i<n;i++){
-            largestArea = max(largestArea, (rightSmall[i] - leftSmall[i] + 1)*heights[i]);
-        }
-        return largestArea;
-        
+        return maxA;
     }
 };
