0537464e0e771d305d468485fddb4895dd7c86fe
Jishu Yadav <54631311+jishu-yadav@users.noreply.github.com>
2022-06-13 23:50:09 +0530

    Time: 14 ms (17.99%), Space: 8.7 MB (70.75%) - LeetHub

--- 120-triangle/120-triangle.cpp
+++ 120-triangle/120-triangle.cpp
@@ -3,23 +3,11 @@ public:
     int minimumTotal(vector<vector<int>>& triangle) {
         int n = triangle.size();
         int m = triangle[0].size();
-        // int dp[n+1];
-        // dp[0] = triangle[n-1][0];
         vector<int>dp(triangle.back());
-    
         for(int i=n-2;i>=0;i--){
-            for(int j=0;j<=i;j++)//moving through the layer and check everynode
-                // Find the lesser of its two children, and sum the current value in the triangle with it.
-            dp[j] = triangle[i][j]+min(dp[j],dp[j+1]);
-//             sum+= min(triangle[i][j],triangle[i][j+1]);
-//             cout<< j<<" "<<sum<<" ";
-//             cout<< triangle[i][j] << " " <<
-//                 triangle<<[i][j+1]<<"\n";
-            
-//             if(triangle[i][j]<triangle[i][j+1])
-//                 j = j+1;
-//             cout<< j<<" "<<sum<<" "<< triangle[i][j]<<" "<<triangle<<[i][j+1]<<"\n";
-            
+            for(int j=0;j<=i;j++){
+                dp[j] = triangle[i][j] + min(dp[j],dp[j+1]);
+            }
         }
         return dp[0];
     }
