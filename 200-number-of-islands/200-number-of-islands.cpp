43b76b810d2ff0ad2ef11f715e6424089d14632d
Jishu Yadav <54631311+jishu-yadav@users.noreply.github.com>
2022-06-23 17:32:20 +0530

    Time: 81 ms (15.81%), Space: 18.2 MB (17.74%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.cpp
+++ 200-number-of-islands/200-number-of-islands.cpp
@@ -2,25 +2,29 @@ class Solution {
 public:
     int numIslands(vector<vector<char>>& grid) {
         //with bfs :!
-       int m = grid.size(), n = m ? grid[0].size() : 0, islands = 0, offsets[] = {0, 1, 0, -1, 0};
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (grid[i][j] == '1') {
+        int islands = 0;
+        int offsets[] = {0,1,0,-1,0};
+        int n = grid.size();
+        int m = grid[0].size();
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(grid[i][j]=='1'){
                     islands++;
                     grid[i][j] = '0';
-                    queue<pair<int, int>> todo;
-                    todo.push({i, j});
-                    while (!todo.empty()) {
-                        pair<int, int> p = todo.front();
+                    queue<pair<int,int>>todo;
+                    todo.push({i,j});
+                    while(!todo.empty()){
+                        pair<int,int>p = todo.front();
                         todo.pop();
-                        for (int k = 0; k < 4; k++) {
-                            int r = p.first + offsets[k], c = p.second + offsets[k + 1];
-                            if (r >= 0 && r < m && c >= 0 && c < n && grid[r][c] == '1') {
-                                grid[r][c] = '0';
-                                todo.push({r, c});
+                        for(int k =0;k<4;k++){
+                            int r  = p.first + offsets[k], c = p.second+offsets[k+1];
+                            if(r>=0 && r<n && c>=0 && c<m && grid[r][c]=='1'){
+                                grid[r][c]='0';
+                                todo.push({r,c});
                             }
                         }
                     }
+                    
                 }
             }
         }
