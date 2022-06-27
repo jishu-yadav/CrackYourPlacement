04bf64a0b1fd89076916e9c8d39a015f88a75886
Jishu Yadav <54631311+jishu-yadav@users.noreply.github.com>
2022-06-27 17:05:48 +0530

    Time: 597 ms (18.49%), Space: 10.5 MB (42.90%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
@@ -1,33 +1,34 @@
 class Solution {
 public:
-    int lengthOfLIS(vector<int>& nums) {
-        
-        int ans=0;
-        int n  = nums.size();
+    // int f(int idx,int prev_idx,vector<int>nums,int n,vector<vector<int>>&dp){
+    //     if(idx==n)
+    //         return 0;
+    //     if(dp[idx][prev_idx+1]!=-1)
+    //         return dp[idx][prev_idx+1];
+    //     int len = 0 + f(idx+1,prev_idx,nums,n,dp);//not take case
+    //     if(prev_idx==-1 || nums[idx] > nums[prev_idx] ){ //take case
+    //         len = max(len, 1 + f(idx+1, idx, nums,n,dp));
+    //     }
+    //     return dp[idx][prev_idx+1] = len;
+    // }
+    // int lengthOfLIS(vector<int>& nums) {
+    //     int n = nums.size();
+    //     vector<vector<int>>dp(n,vector<int>(n+1,-1));
+    //     return f(0,-1,nums,n,dp);
+    // }
+     int lengthOfLIS(vector<int>& nums) {
+        int ans = 1;
+        int n = nums.size();
         vector<int>dp(n,1);
-        for(int i=0;i<n;i++){
-            for(int j=0;j<i;j++){
-                if(nums[i] > nums[j] && dp[i]<dp[j]+1)
-                    dp[i] = dp[j] + 1;
-            }
-        }
-        return *max_element(dp.begin(),dp.end());
-        
-        // for(int i =0;i<n;i++){
-        //     int c=0;
-        //     for(int j = i+1;j<n;j++){
-        //         if(nums[j]>nums[i]){
-        //             c++;
-        //             cout<<nums[j]<<" ";
-        //         }
-        //     }
-        //     cout<<"\n";
-        //    // cout<<c<<" ";
-        //     if(c>ans)
-        //         ans = c;
-        // }
-        // return ans;
-        
-        
+         for(int i=0;i<n;i++)
+         {
+             for(int j=0;j<i;j++){
+                 if(nums[i]>nums[j]){
+                     dp[i] = max(dp[i],dp[j]+1);
+                     ans = max(ans,dp[i]);
+                 }
+             }
+         }    
+             return ans;
     }
 };
