9cd720772b4e79075bc8be2b085972a54adf885a
Jishu Yadav <54631311+jishu-yadav@users.noreply.github.com>
2022-07-28 10:23:06 +0530

    Time: 3 ms (98.72%), Space: 7.1 MB (94.64%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.cpp
+++ 242-valid-anagram/242-valid-anagram.cpp
@@ -1,8 +1,21 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        sort(s.begin(),s.end());
-        sort(t.begin(),t.end());
-        return s==t;
+        // sort(s.begin(),s.end());
+        // sort(t.begin(),t.end());
+        // return s==t;
+        vector<int>hash(26,0);
+        for(int i=0;i<s.size();i++){
+            hash[s[i]-'a']++;
+        }
+        for(int i=0;i<t.size();i++){
+            hash[t[i]-'a']--;
+        }
+        for(int i=0;i<hash.size();i++){
+            if(hash[i]!=0){
+                return false;
+            }
+        }
+        return true;
     }
 };
