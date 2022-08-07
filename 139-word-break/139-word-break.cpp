1bc19b5a839cf070ae5c1bdca46e9a1a68bec4cd
Jishu Yadav <54631311+jishu-yadav@users.noreply.github.com>
2022-08-07 10:20:45 +0530

    Time: 10 ms (71.68%), Space: 8 MB (82.28%) - LeetHub

--- 139-word-break/139-word-break.cpp
+++ 139-word-break/139-word-break.cpp
@@ -1,42 +1,29 @@
 class Solution {
 public:
     int dp[301];
-    // Below code gave tle and only 35/45 tcs passed
-        
-    // int help(int i,string s,set<string>&wordDict){
-    //     if(i==s.size())
-    //         return 1;
-    //     string temp;
-    //     for(int j=i;j<s.size();j++){
-    //         temp+=s[j];
-    //         if(wordDict.find(temp)!=wordDict.end()){
-    //             if(help(j+1,s,wordDict))
-    //                 return 1;
-    //         }
-    //     }
-    //     return 0;
-    // }
-    int help(int i,string s,set<string>&wordDict){
+    int helper(int i,string &s,set<string>&st){
         if(i==s.size())
             return 1;
         if(dp[i]!=-1)
-                return dp[i];
+            return dp[i];
         string temp;
         for(int j=i;j<s.size();j++){
             temp+=s[j];
-            if(wordDict.find(temp)!=wordDict.end()){
-                if(help(j+1,s,wordDict))
-                    return dp[i]=1;
+            if(st.find(temp)!=st.end()){
+                if(helper(j+1,s,st)){
+                    return dp[i] = 1;
+                }
             }
         }
         return dp[i] = 0;
     }
     bool wordBreak(string s, vector<string>& wordDict) {
+        int i=0;
         set<string>st;
         for(auto i:wordDict){
             st.insert(i);
         }
         memset(dp,-1,sizeof(dp));
-        return help(0,s,st);
+        return helper(i,s,st);
     }
 };
