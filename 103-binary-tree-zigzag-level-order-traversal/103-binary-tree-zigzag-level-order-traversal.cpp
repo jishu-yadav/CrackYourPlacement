c8a1e5f2a1b8bb91b9e0618ac24ca4d199631a56
Jishu Yadav <54631311+jishu-yadav@users.noreply.github.com>
2022-08-15 12:55:02 +0530

    Time: 8 ms (27.80%), Space: 11.9 MB (99.12%) - LeetHub

--- 103-binary-tree-zigzag-level-order-traversal/103-binary-tree-zigzag-level-order-traversal.cpp
+++ 103-binary-tree-zigzag-level-order-traversal/103-binary-tree-zigzag-level-order-traversal.cpp
@@ -12,41 +12,35 @@
 class Solution {
 public:
     vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
-//         stack<TreeNode*>stk1;
-//         stack<TreeNode*>stk2;
-        
-//         vector<vector<int>>ans;
-//         stk1.push(root);
-        
-//         while(stk2.empty()){
-//             ans.push_back(root->val);
-            
-            
-//         }
-        vector<vector<int>>result;
-        if(root==NULL)
-            return result;
-        queue<TreeNode*>nodesQueue;
-        nodesQueue.push(root);
-        bool leftToRight = true;
+       vector < vector < int >> result;
+  if (root == NULL) {
+    return result;
+  }
+
+  queue < TreeNode * > nodesQueue;
+  nodesQueue.push(root);
+  bool leftToRight = true;
+        //int len=0;
         while(!nodesQueue.empty()){
             int size = nodesQueue.size();
             vector<int>row(size);
             for(int i=0;i<size;i++){
-                TreeNode* node = nodesQueue.front();
+                TreeNode* node =nodesQueue.front();
                 nodesQueue.pop();
-                int index = leftToRight ? i:size-1-i;
-                row[index] = node->val;
-                if(node->left)
+                int index = (leftToRight) ? i : (size - 1 - i);
+                row[index] = (node->val);
+                
+                if(node -> left){
                     nodesQueue.push(node->left);
-                if(node->right)
+                }
+                if(node -> right){
                     nodesQueue.push(node->right);
+                }
                 
             }
             leftToRight = !leftToRight;
-            result.push_back(row);
-            
+             result.push_back(row);
         }
-        return result;
+        return  result;
     }
 };
