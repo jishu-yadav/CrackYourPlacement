e8f3cd6d1995a8ba8f18312d3477348ae5953ef1
Jishu Yadav <54631311+jishu-yadav@users.noreply.github.com>
2022-08-21 16:48:56 +0530

    Time: 39 ms (41.89%), Space: 14.3 MB (42.34%) - LeetHub

--- 207-course-schedule/207-course-schedule.cpp
+++ 207-course-schedule/207-course-schedule.cpp
@@ -1,34 +1,30 @@
 class Solution {
 public:
-bool dfs(int i,vector<int>&visited,vector<int>adj[]){
-        if(visited[i] == 1)
-            return true;
-        if(visited[i]==0){
-            visited[i] = 1;
-            for(auto edge:adj[i]){
-                if(dfs(edge,visited,adj)){
-                    return true;
-                }
+    bool dfs(int i, vector<int> &visited,vector<int>adj[]){
+        if(visited[i]==2)
+            return false;    
+        visited[i] = 1;
+        for(auto edge:adj[i]){
+            if(visited[edge]==1){
+                return true;
             }
-        }
+            if(dfs(edge,visited,adj))
+                return true;
+        }        
         visited[i] = 2;
         return false;
-        
     }
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
         int n = numCourses;
+        vector<int>visited(n,0);
         vector<int>adj[n];
-        for(auto pre:prerequisites){//edges insertion
+        for(auto pre:prerequisites){
             adj[pre[1]].push_back(pre[0]);
         }
-        
-        vector<int>visited(n,0);
-        
         for(int i=0;i<n;i++){
-            if(dfs(i,visited,adj)){
+            if(visited[i]==0 and dfs(i,visited,adj))
                 return false;
-            }
         }
-        return true;   
+        return true;
     }
 };
