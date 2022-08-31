39438a404a6fe42ff8272d56b47dfd06fbbcaf11
Jishu Yadav <54631311+jishu-yadav@users.noreply.github.com>
2022-08-31 17:27:35 +0530

    Time: 106 ms (77.16%), Space: 11.2 MB (80.86%) - LeetHub

--- 322-coin-change/322-coin-change.cpp
+++ 322-coin-change/322-coin-change.cpp
@@ -1,45 +1,29 @@
 class Solution {
 public:
-    int dp[12+1][10001];
-    // int helper(int n,vector<int>&coins,int target){
-    //     if(target==0 || n==0){
-    //         return 0;
-    //     }
-    //     if(coins[n-1]<=target){
-    //         cout<< min(1 + helper(n-1,coins,target-coins[n-1]), helper(n,coins,target) );
-    //         return min(1 + helper(n-1,coins,target-coins[n-1]), helper(n,coins,target) );
-    //     }
-    //     return helper(n-1,coins,target);
-    // }
-     int helper(int i,vector<int>&coins,int target){
-        if(target==0 || i==0){
-            return target==0?0:INT_MAX-1;
-        }
+    int dp[12+1][10000+1];
+    int helper(int i, vector<int>&coins,int target){
+        if(i==0 || target==0)
+            return target==0?0:INT_MAX-1; 
         if(dp[i][target]!=-1)
             return dp[i][target];
         int res = -1;
-         
         if(coins[i-1]<=target){
-            int take = 1 + helper(i,coins,target-coins[i-1]);
-            int doNotTake = 0 + helper(i-1,coins,target);
-            dp[i][target] = res = min(take,doNotTake);
+            int take = 1+helper(i,coins,target-coins[i-1]);
+            int notTake =0+ helper(i-1,coins,target);
+            dp[i][target] = res = min(take, notTake);
+        }else{
+             int notTake = helper(i-1,coins,target);
+             res  = notTake;
         }
-        else
-         {
-            int doNotTake = helper(i-1,coins,target);
-            res = doNotTake;
-         }
         return dp[i][target] = res;
     }
     int coinChange(vector<int>& coins, int amount) {
+        int n = coins.size();
         memset(dp,-1,sizeof(dp));
-        int res = helper(coins.size(),coins,amount);
-       
-        
+        int res = helper(n,coins,amount);
         if(res==INT_MAX-1)
             return -1;
         else
             return res;
-        
     }
 };
