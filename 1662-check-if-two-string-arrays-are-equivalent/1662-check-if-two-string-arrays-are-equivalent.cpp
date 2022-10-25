6cbe26496b2d34bc767b7596e853c89265aab158
Jishu Yadav <54631311+jishu-yadav@users.noreply.github.com>
2022-10-25 22:15:57 +0530

    Time: 10 ms (44.93%), Space: 11.3 MB (89.94%) - LeetHub

--- 1662-check-if-two-string-arrays-are-equivalent/1662-check-if-two-string-arrays-are-equivalent.cpp
+++ 1662-check-if-two-string-arrays-are-equivalent/1662-check-if-two-string-arrays-are-equivalent.cpp
@@ -8,9 +8,7 @@ public:
         for(int i=0;i<word2.size();i++){
             s2+=word2[i];
         }
-        // sort(s1.begin(),s1.end());
-        // sort(s2.begin(),s2.end());
-        cout<<s1<<" "<<s2<<" ";
+        //cout<<s1<<" "<<s2<<" ";
         if(s1.size()!=s2.size())
             return false;
         for(int i=0;i<s1.size();i++){
