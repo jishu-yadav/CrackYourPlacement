52e668b6509bf51445af8dad43d11663956fe063
Jishu Yadav <54631311+jishu-yadav@users.noreply.github.com>
2022-12-08 23:03:28 +0530

    Time: 6 ms (91.15%), Space: 7.2 MB (10.48%) - LeetHub

---
+++ 0205-isomorphic-strings/0205-isomorphic-strings.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+   
+     bool helper(string s,string t){
+        int n = s.size();
+        map<char,char>mp;
+        for(int i=0;i<n;i++){
+            if(mp.find(s[i])!=mp.end()){
+                if(mp[s[i]]!=t[i]){
+                    return false;
+                }
+            }
+            else{
+                mp[s[i]] = t[i];
+            }
+        }
+        return true;
+    }
+    bool isIsomorphic(string s, string t) {
+        return helper(s,t)&&helper(t,s);
+    }
+};
