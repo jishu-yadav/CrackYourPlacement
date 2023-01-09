c1eb3245bfb203cf3688898fcec698056ddc1d51
Jishu Yadav <54631311+jishu-yadav@users.noreply.github.com>
2023-01-09 23:22:18 +0530

    Time: 34 ms (62.01%), Space: 31.2 MB (56.86%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.cpp
+++ 0724-find-pivot-index/0724-find-pivot-index.cpp
@@ -1,33 +1,19 @@
 class Solution {
 public:
     int pivotIndex(vector<int>& nums) {
-//         int i=0,n=nums.size();
-//         vector<int>l(n,0);
-//         vector<int>r(n,0);
-//         int s=0,left=0;
-//         int sl=0;
-//         int sr=0;
-//         int ans = -1;
-//         for(i=0;i<n;i++){
-//             sl+=nums[i];
-//             l.push_back(sl);
-//             s+=nums[i];
-//         }
-//          for(i=0;i<n;i++){
-             
-//             if(left== s - left-nums[i]){
-//                 ans = i;
-//             }
-//              left+=nums[i];
-//         }
-//         return ans;
-         int total = 0;
-        for (int num : nums) total += num;
-        int sum = 0;
-        for (int i = 0; i < nums.size(); sum += nums[i++])
-            if (sum * 2 == total - nums[i])
+           int prefix_sum  =0;
+        for(int i= 0; i < nums.size(); i++){
+            prefix_sum += nums[i];
+        }
+        int left_sum =0; 
+        int right_sum = prefix_sum ;
+        for(int i= 0; i < nums.size(); i++){
+            right_sum = right_sum - nums[i];
+            if(left_sum == right_sum)
                 return i;
-        
+            
+            left_sum += nums[i];
+        }
         return -1;
     }
 };
